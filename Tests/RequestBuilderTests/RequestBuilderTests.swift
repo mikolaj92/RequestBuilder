4d94d827d58470aa4ec0c10e4dbbc0356a3abc12
Patryk Mikolajczyk <p.mikolajczyk@bsgroup.eu>
2020-02-19 08:31:54 +0100

    tests

--- Tests/RequestBuilderTests/RequestBuilderTests.swift
+++ Tests/RequestBuilderTests/RequestBuilderTests.swift
@@ -2,10 +2,60 @@ import XCTest
 @testable import RequestBuilder
 
 final class RequestBuilderTests: XCTestCase {
-    func testExample() {
+    private enum Constants {
+        static let baseURL = "jsonplaceholder.typicode.com"
+        static let userId = 1
+    }
+    
+    private struct PostRequest: ServiceRequestProtocol {
+        let urlHost: String = Constants.baseURL
+        let urlPath: String = "/posts"
+        let httpMethod: HTTPMethod = .post
+        let parameters: RequestParameters? = .json(PostParams(userId: 1))
+    }
+	
+	struct GetRequest: ServiceRequestProtocol {
+		let urlHost: String = "jsonplaceholder.typicode.com"
+		let urlPath: String = "/posts"
+		var parameters: RequestParameters? {
+		  return .urlEncoded(["userId":"\(userId)"])
+		}
+		let userId: Int
+	}
+
+    
+    private struct PostParams: Codable, Equatable {
+        let userId: Int
+    }
+    
+    func testGetRequest() {
+        let request = GetRequest(userId: Constants.userId).request
+        XCTAssertEqual(request.url, URL(string: "https://jsonplaceholder.typicode.com/posts?userId=\(Constants.userId)"))
+    }
+    
+    func testGetCurl() {
+        let request = GetRequest(userId: Constants.userId)
+        XCTAssertEqual(request.curlString, "curl https://jsonplaceholder.typicode.com/posts?userId=\(Constants.userId)")
+    }
+    
+    func testPostRequest() {
+        let request = PostRequest().request
+        XCTAssertEqual(request.url, URL(string: "https://jsonplaceholder.typicode.com/posts"))
+        XCTAssertEqual(request.httpBody, PostParams(userId: Constants.userId).encoded)
+    }
+    
+    func testPostCurl() {
+        let request = PostRequest()
+        let curl = request.curlString
+        XCTAssertTrue(curl.contains("curl https://jsonplaceholder.typicode.com/posts"))
+        XCTAssertTrue(curl.contains("-d '{\"userId\":1}'"))
+        XCTAssertTrue(curl.contains("-X POST"))
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testGetRequest", testGetRequest),
+		("testGetCurl", testGetCurl),
+		("testPostRequest", testPostRequest),
+		("testPostCurl", testPostCurl),
     ]
 }
